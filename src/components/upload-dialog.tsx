2626eebd86a1736144e593be0099ff36b0c851a7
DaveFlashNL <72280178+DaveFlashNL@users.noreply.github.com>
2022-01-16 22:56:25 +0100

    fixed some wording
    zapped the gremlins

--- src/components/upload-dialog.tsx
+++ src/components/upload-dialog.tsx
@@ -109,7 +109,7 @@ export const UploadDialog = (props: {}) => {
             aria-labelledby="alert-dialog-slide-title"
             aria-describedby="alert-dialog-slide-description"
         >
-            <DialogTitle id="alert-dialog-slide-title">Recording or downloading...</DialogTitle>
+            <DialogTitle id="alert-dialog-slide-title">Processing...</DialogTitle>
             <DialogContent>
                 <DialogContentText id="alert-dialog-slide-description">
                     {convertedValue === 100 && trackConverting === trackTotal
