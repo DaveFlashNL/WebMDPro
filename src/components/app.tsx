f1d850f0ad2094564f35ec1e199da9f596e3d0b2
Dave Wouters <ddw@ddw.nu>
2022-01-19 12:33:25 +0100

    increment

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -145,7 +145,7 @@ const App = () => {
                         </Link>{' '}
                         {new Date().getFullYear()}
                         {'.'}<br /><br />
-                        {'(v0.3.0 ~ 7f4cc4a1)'}<br />
+                        {'(v0.3.1 ~ ba245ec3)'}<br />
                         <sub>{'*Disclaimers moved to about box'}</sub>
                     </Typography>
                 </main>
